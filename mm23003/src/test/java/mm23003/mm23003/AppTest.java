3e0f0e58eee80a5ccf363eb1e7227a4dd4af1423
michcv <mm23003@ues.edu.sv>
2024-05-02 08:48:57 -0600

    My first commit

---
+++ mm23003/src/test/java/mm23003/mm23003/AppTest.java
@@ -0,0 +1,20 @@
+package mm23003.mm23003;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello()
+    {
+        assertTrue( true );
+    }
+}
